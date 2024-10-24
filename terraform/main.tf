585a2c08f0a16cc486d4d8a440902fba80727273
Ubuntu <azureuser@VM2.xqdhfuyc0mqurl1jlhbstrzs0e.fx.internal.cloudapp.net>
2024-10-24 12:56:21 +0000

    terraform updated, pipelines updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,29 +1,32 @@
 provider "azurerm" {
-    features = {}
+  features = {}
 }
 
-#My resource group is ResourceGroup2
+# My resource group is ResourceGroup2
 data "azurerm_resource_group" "existing_rg" {
-  name = "ResourceGroup2" 
+  name = "ResourceGroup2"
 }
 
-#Service plan for AppService
-resource "azurerm_app_service_plan" "asp" [
-  name			= "asp-web-app-cicd"
-  location		= data.azurerm_resource_group.existing_rg.location
-  resource_group_name	= data.azurerm_resource_group.existing_rg.name
-sku {
+# Service plan for App Service
+resource "azurerm_app_service_plan" "asp" {
+  name                = "asp-web-app-cicd"
+  location            = data.azurerm_resource_group.existing_rg.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  
+  sku {
     tier = "Free"
     size = "F1"
   }
 }
-#AppService creation
-name                = "webapp-cicd"
+
+# App Service creation
+resource "azurerm_app_service" "app_service" {
+  name                = "webappcicd"  
   location            = data.azurerm_resource_group.existing_rg.location
   resource_group_name = data.azurerm_resource_group.existing_rg.name
   app_service_plan_id = azurerm_app_service_plan.asp.id
 
   site_config {
-    linux_fx_version = "DOCKER|<image_name>"
+    linux_fx_version = "DOCKER|webappcicd.azurecr.io/flask-app:latest"  # Înlocuiește <image_name> cu imaginea corectă
   }
 }
